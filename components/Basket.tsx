9e489877310a0fe083e1c8ec99bb6856d8297f59
NabilRafiq <nabilrafiqmugen@gmail.com>
2021-02-06 03:50:37 -0800

    Shopping Basket

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -14,6 +14,8 @@ import {
 import DeleteIcon from "@material-ui/icons/Delete"
 
 import { ProductItem } from "../global"
+import { useSelector } from "react-redux"
+import { store, remove } from "../store"
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -37,7 +39,7 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const Basket = () => {
   const classes = useStyles({})
-  const products = [] // TODO
+  const products = useSelector((state: ProductItem[]) => state)
 
   return (
     <>
@@ -76,9 +78,7 @@ const Basket = () => {
                   <IconButton
                     edge="end"
                     aria-label="delete"
-                    onClick={() => {
-                      /* Remove from basket */
-                    }}
+                    onClick={() => store.dispatch(remove({ id: product.id }))}
                   >
                     <DeleteIcon />
                   </IconButton>
